ab13cef8d229fb829e2b1cf2c9d854c0f50ecb9d
Poisonlocket <loribono@gmx.ch>
2025-07-01 21:44:34 +0200

    moved content.config.ts to src so it work, modified the schema so multiple authors are allowed, 'mental illness is a journey and it starts here'

--- src/content.config.ts
+++ src/content.config.ts
@@ -3,22 +3,23 @@ import {glob, file} from "astro/loaders";
 
 
 const posts = defineCollection({
-    loader: glob({pattern: "*.md", base: "./posts"}),
+    loader: glob({pattern: "*.md", base: "src/content/posts"}),
     schema: z.object({
         title: z.string().max(50),
         description: z.string().max(200),
-        author: z.string().max(30),
+        authors: z.array(z.string().max(30)),
         reading: z.number().int(),
         topicTags: z.array(z.string()),
-        date: z.string(),
+        date: z.string(), // or z.date() if you want native Date parsing
         relatedArticles: z.array(z.string()),
         previewImagePath: z.string()
     })
 });
 
 const authors = defineCollection({
-    loader: file("./data/authors.json"),
+    loader: file("src/content/authors/authors.json"),
     schema: z.object({
+        slug: z.string().max(30),
         name: z.string(),
         username: z.string(),
         description: z.string(),
@@ -27,8 +28,8 @@ const authors = defineCollection({
         github: z.string(),
         website: z.string(),
         profile_image: z.string(),
+
     })
 })
 
-
-
+export const collections = {posts, authors};
--- src/content/posts/omen.md
+++ src/content/posts/omen.md
@@ -1,10 +1,10 @@
 ---
 title: "Introduction to Serverless Architecture"
 description: "A comprehensive guide to understanding and using serverless computing to build scalable, cost-effective applications."
-author: "Alice Johnson"
-reading_time: 8
-tags: [ "Cloud", "Serverless", "AWS", "DevOps",]
+author: ["Alice Johnson"]
+reading: 8
+topicTags: [ "Cloud", "Serverless", "AWS", "DevOps",]
 date: "2025-06-15"
-related_articles: [ "Building Scalable APIs with Node.js", "Deploying on AWS: Best Practices"]
-preview_image_path: "/images/articles/serverless-preview.jpg"
+relatedArticles: [ "Building Scalable APIs with Node.js", "Deploying on AWS: Best Practices"]
+previewImagePath: "/images/articles/serverless-preview.jpg"
 ---
